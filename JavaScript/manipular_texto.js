25fd7382d4e719f13f5d671237500b1fe9d22416
Helena-Siqueira <156859073+Helena-Siqueira@users.noreply.github.com>
2025-07-29 20:54:54 -0300

    Atividade Carrinho Finalizada, conteúdo novo IF ELSE

---
+++ JavaScript/manipular_texto.js
@@ -0,0 +1,69 @@
+
+// let idade = parseInt(prompt("Digite sua idade: "))
+
+// if( idade >= 17){
+//     alert("Você já pode tirar CNH")
+// }
+// else{
+//     alert("Você ainda não tem idade para tirar CNH...")
+// }
+
+
+// let numero1 = parseInt(prompt("Digite um número"));
+// let numero2 = parseInt(prompt("Digite outro número "));
+
+// if( numero1 < numero2){
+//     alert("Ordem crescente: " +numero1+ " , " +numero2)
+// }
+// else{
+//     alert("Ordem crescente: " +numero2+ " , " +numero1)
+// }
+
+
+
+// let numero = prompt(("Digite um numero: "))
+
+// if(numero > 0 ){
+//     alert("Este número é positivo: " +numero)
+// } else{
+//     alert("Este número é negativo: "+numero)
+//      }
+
+
+// let nota1 = parseInt(prompt(" Digite a primeira nota do aluno: "));
+// let nota2 = parseInt(prompt(" Digite a segunda nota do aluno: "));
+// let media = (nota1 + nota2) / 2
+
+// if(media >= 7){
+//     alert("Aluno aprovado: " +media)
+// }else{
+//    alert("Aluno reprovado: "+media)
+// }
+
+
+// function atividadeHotel(){
+    
+//     let numero = parseInt(prompt("Digite o numero 3"))
+
+// if(numero == 3){
+//     alert("Parabéns você digitou o número correto!")
+
+// }else{
+//     alert("Número inválido...")
+//     return // para rodar o sistema.
+// }
+
+// let resposta = prompt("Digite batata")
+// if(resposta == "batata" ){
+//     alert("Parabéns você digitou a resposta correta")
+// }else{
+//     alert("Resposta inválida")
+// }
+
+// alert("Fim do sistema")
+
+// }
+
+
+
+
